b6947fb6592db241fd12a9f77b176d24e4afb8a0
maxcostatismart <max.costa@tismart.com>
2022-04-17 17:31:56 -0500

    test heroku deploy 5

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/capstoneaf-frontend'));
+app.use(express.static('./dist/sowad-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/capstoneaf-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/sowad-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
