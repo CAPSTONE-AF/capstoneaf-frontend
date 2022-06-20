d0a576d2cd9efb26aa1a36f78450cff47d5fe184
maxcostatismart <max.costa@tismart.com>
2022-06-20 18:54:05 -0500

    GOOGLE ANAYLITICS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'MEASUREMENT-ID', { 'page_path': event.urlAfterRedirects });
+      }
+    })
+  }
+}
